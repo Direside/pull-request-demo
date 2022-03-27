869fe22140106a987114ec05c17b944e8f4acf1a
Shane Gearon <shane.gearon@gmail.com>
2022-03-27 09:50:17 -0700

    Fixing linter errors maybe?

--- fileWeAllUse.py
+++ fileWeAllUse.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Main Route for the app
 @app.route('/')
 def index():
     return 'Hello World!'
 
+# Signup page
 @app.route('/signup')
 def signup():
     return 'Please create an account here.'
- 

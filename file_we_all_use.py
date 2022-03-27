019028cd6ad8122dce5af26e387e68fa3f41e05a
Shane Gearon <shane.gearon@gmail.com>
2022-03-27 09:53:49 -0700

    Adding docstring

--- file_we_all_use.py
+++ file_we_all_use.py
@@ -6,12 +6,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Main Route for the app
+"""
+Main method for app
+"""
 @app.route('/')
 def index():
     return 'Hello World!'
 
-# Signup page
+"""
+Signup Page
+"""
 @app.route('/signup')
 def signup():
     return 'Please create an account here.'
